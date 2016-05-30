96288400f3566d6696c6d47d18c88b701c996b85
William Swinny <williamwswinny@gmail.com>
2016-05-30 17:45:33 -0500

    The python script can now play audio YAY :D

--- musicManagerDaemon.py
+++ musicManagerDaemon.py
@@ -3,12 +3,27 @@ import thread
 import threading
 import time
 import MySQLdb
+import os
+import sys
 import pygame
+import pygame.display
+
+#set graphics driver to dummy for headless display
+#WARNING: This line makes it so you have to run the script with sudo
+os.environ["SDL_VIDEODRIVER"] = "dummy"
+
+#init pygame
+pygame.init()
+pygame.mixer.init()
+
+pygame.display.init()
+screen = pygame.display.set_mode((1,1))
 
 #connect to database and setup cursor for queries
 db = MySQLdb.connect(host="localhost", user="root", passwd="XDR%xdr5CFT^cft6", db="MusicManager")
 cur = db.cursor()
 
+#where the music is stored
 musicDirectory = "/home/pi/Music/"
 SONGEND = pygame.USEREVENT + 1
 
@@ -74,14 +89,17 @@ def playsong(songID):
 
 	songpath = musicDirectory + artist + "/" + song
 
+	#plays the song 1 time using the pygame audio lib
 	pygame.mixer.music.load(songpath)
 	pygame.mixer.music.play(0)
-	pygame.mixer.music.set_endevent(SONGEND)
+	pygame.mixer.music.set_endevent(SONGEND) #fires event when the song has stopped playing
 
 def runSongThread():
 	playsong(2)
 
 	while True:
+
+		#pull for events using pygame
 		for event in pygame.event.get():
 			if event.type == SONGEND:
 				print "Music Stoped playing"
@@ -89,7 +107,5 @@ def runSongThread():
 		time.sleep(0.5)
 
 if __name__ == "__main__":
-	pygame.mixer.init()
-
 	runSongThread()
 	db.close()
