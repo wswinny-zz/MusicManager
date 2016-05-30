cfaa5d8bff8dfe997e85af99db28351873cc3f12
William Swinny <williamwswinny@gmail.com>
2016-05-30 16:55:24 -0500

    Merge with alex

--- musicManagerDaemon.py
+++ musicManagerDaemon.py
@@ -4,14 +4,19 @@ import threading
 import time
 import MySQLdb
 
+from pygame import mixer
+
 #connect to database and setup cursor for queries
 db = MySQLdb.connect(host="localhost", user="root", passwd="XDR%xdr5CFT^cft6", db="MusicManager")
 cur = db.cursor()
 
+musicDirectory = "/home/pi/Music/"
+
 songQueue = list()		#Holds song but in what form name, id?
-playSet	  = set()		#Holds elements of songQueue to be played
+playSet	  = set([1,2,3,4,5,6,7,8,9])		#Holds elements of songQueue to be played
+
 suffle    = False		#true - on, false - off
-repeat    = True		#true - on, false - off
+repeat    = False		#true - on, false - off
 
 thePeoplesMutex = threading.Lock()
 
@@ -49,14 +54,17 @@ def pause():
 def resume():
 	return
 
+def playsong(songID):
+	result = execQuery("SELECT artistName, songName FROM Song LEFT JOIN Artist ON Song.artist = Artist.idArtist WHERE idSong = " + str(songID) + ";")
+	artist = result[0][0]
+	song = result[0][1]
+
+	return musicDirectory + artist + "/" + song
+
 def runSongThread():
-	return
+	while True:
+			
 
 if __name__ == "__main__":
-	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	s.bind(('',42069))
-
-	data,address = socket.recvfrom(16)
-	print "recv: ", data
-
+	playsong(2)
 	db.close()
