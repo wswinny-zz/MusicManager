0def1521d62376d3ee570157cc7222749f4e6fb0
William Swinny <williamwswinny@gmail.com>
2016-05-30 14:28:25 -0500

    Added python script layout

---
+++ musicManagerDaemon.py
@@ -0,0 +1,34 @@
+import socket
+import thread
+import time
+import MySQLdb
+
+def addSong(songID):
+	return
+
+def addPlaylist(playlistID):
+	return
+
+def clear():
+	return
+
+def skip(direction):
+	return
+
+def shuffle(isShuffling):
+	return
+
+def repeat(doRepeat):
+	return
+
+def pause():
+	return
+
+def resume():
+	return
+
+def runSongThread():
+	return
+
+if __name__ == "__main__":
+	return
